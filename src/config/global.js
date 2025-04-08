0081dada1f79f12f73ed3ec2e12967d7c4b4bebd
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-04-08 14:17:12 -0500

    Vinculación de PDF y MD

--- src/config/global.js
+++ src/config/global.js
@@ -117,7 +117,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/72210002_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
